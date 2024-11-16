a3e1e17f70cb2908b812182c54427a8607371f5b
Thomas Beck <thomas@beckshome.com>
2024-11-16 23:07:45 +0000

    added facet results initializer

--- 4-Faceting/Shared/SearchModel.cs
+++ 4-Faceting/Shared/SearchModel.cs
@@ -10,6 +10,6 @@ namespace search.Shared
         public int PageCount {get; set;}
         public int CurrentPage {get; set;}
         public List<WaffleText> CurrentPageSearchResults {get; set;} = new List<WaffleText>();
-        public IList<FacetResult> FacetResults { get; set; }
+        public IList<FacetResult> FacetResults { get; set; } = new List<FacetResult>();
     }
 }
